bba97f8ac52389fd267b7cc0e246327e88b193e9
Oxidiz3 <mecham.porter@gmail.com>
2025-11-07 13:29:47 -0700

    Add log for if you're ahead of main as well

--- scripts/IsGitConfigCurrent.ps1
+++ scripts/IsGitConfigCurrent.ps1
@@ -1,6 +1,9 @@
 git fetch origin 2>$null
-$behind = (git rev-list --left-right --count origin/main...HEAD).Split()[0]
+$behind, $ahead = (git rev-list --left-right --count origin/main...HEAD).Split()
 
 if ($behind -gt 0) {
     Write-Output "Your repo is behind by $behind commits!"
+} 
+if ($ahead -gt 0) {
+    Write-Output "Your repo is ahead by $ahead commits!"
 }
