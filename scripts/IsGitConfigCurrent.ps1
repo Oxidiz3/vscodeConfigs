ca40ce0214b3b77dc21a0a06dbaea71b04a8f5ab
Oxidiz3 <57640992+Oxidiz3@users.noreply.github.com>
2025-12-08 11:44:53 -0700

    12/8/2025 Added new whichkey commands for copilot and document. Also added the runTerminalCommands plugin that calls different scripts for me. Also updated isGitConfigCurrent.ps1 to alert when the git is dirty (#7)
    Co-authored-by: Porter Mecham <pmecham@Melaleuca.com>

--- scripts/IsGitConfigCurrent.ps1
+++ scripts/IsGitConfigCurrent.ps1
@@ -1,4 +1,4 @@
-git fetch origin main2>$null
+git fetch origin main 2>$null
 $behind, $ahead = (git rev-list --left-right --count origin/main...HEAD).Split()
 
 if ($behind -gt 0) {
@@ -6,4 +6,10 @@ if ($behind -gt 0) {
 } 
 if ($ahead -gt 0) {
     Write-Output "Your repo is ahead by $ahead commits!"
+}
+
+# Alert if there are uncommitted changes in the working tree
+$dirty = git status --porcelain 2>$null
+if ($dirty) {
+    Write-Output "You have uncommitted changes in your working directory!"
 }
